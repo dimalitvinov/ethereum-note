af2f3fe21905297c866beafd2fa8e7e784eac70e
ismwh <dima8litvinov@gmail.com>
2017-09-15 00:09:28 +0400

    fix truffle config

--- truffle.js
+++ truffle.js
@@ -3,7 +3,12 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*"
+    },
+    prod: {
+      host: "91.201.41.52",
+      port: 8545,
+      network_id: "*"
     }
   }
 };
